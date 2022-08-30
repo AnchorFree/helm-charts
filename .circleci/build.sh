798d99b769ddf68948893372b53518cedec8be47
Aleksey Zhukov <353748+alezkv@users.noreply.github.com>
2022-08-30 14:27:43 +0300

    DEVOPS-8577 Change user

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -6,8 +6,8 @@ FOLDER="packages"
 mkdir -p ${FOLDER}
 helm init --client-only
 
-git config --global user.email "y.molochko@anchorfree.com"
-git config --global user.name "Yaroslav Molochko"
+git config --global user.email "1231870+circleci[bot]@users.noreply.github.com"
+git config --global user.name "CircleCI[bot]"
 git checkout gh-pages
 git rebase master
 # build each chart separately
